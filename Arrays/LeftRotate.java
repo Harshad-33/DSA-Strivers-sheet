4825380e8ac91945ba822335bec39aaa296af31f
Harshad Porajwar <harshadporajwar21@gmail.com>
2026-02-11 17:16:11 +0530

    Rotate_Array_k_times

---
+++ Arrays/LeftRotate.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class LeftRotate {
+    public static void RotateArrayLeft(int[] nums , int k) {
+        int n = nums.length;
+        k = k%n;
+        RotateArray(nums, 0, n-1);
+        RotateArray(nums, 0, k-1);
+        RotateArray(nums, k, n-1);
+    }
+
+    public static void RotateArray(int []nums , int start , int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 1;
+        RotateArrayLeft(nums, k);
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
+
+
