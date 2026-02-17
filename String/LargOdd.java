82c211ef1931ebd0ed3aeb16a68fc9c36fd25ef7
Harshad Porajwar <harshadporajwar21@gmail.com>
2026-02-18 03:42:50 +0530

    Largest odd number in String

---
+++ String/LargOdd.java
@@ -0,0 +1,19 @@
+package String;
+
+public class LargOdd {
+    public static String largestOddNumber(String num) {
+        int n = num.length();
+        for(int i=n-1;i>=0;i--){
+            char ch = num.charAt(i);
+            if((ch - '0') % 2 == 1){
+                return num.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+    public static void main(String[] args) {
+        String num = "35427";
+        System.out.println(largestOddNumber(num));
+
+    }
+}
