685d5ad058ed9d0e2c15574096ef5b4474abf437
Harshad Porajwar <harshadporajwar21@gmail.com>
2026-04-27 23:00:26 +0530

    Middle of LinkedList

---
+++ LinkedList/MiddleNode.java
@@ -0,0 +1,42 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class MiddleNode {
+    public static ListNode middleNode(ListNode head) {
+        if(head == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        ListNode temp = middleNode(head);
+        while (temp != null) {
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+
+    }
+}
+
+
+
