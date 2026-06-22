a03049b7f455f803059e4c4d561eeedf305db857
Harshad Porajwar <harshadporajwar21@gmail.com>
2026-06-22 13:14:14 +0530

    Count Good numbers

--- Recursion/CountGN.java
+++ Recursion/CountGN.java
@@ -1,7 +1,7 @@
 package Recursion;
 
 public class CountGN {
-    long MOD = 1000000007;
+    static long MOD = 1000000007;
     public static int CountGoodNumbers(long n){
         long even = (n+1)/2;
         long odd = (n/2);
@@ -12,22 +12,22 @@ public class CountGN {
         return (int)((evenways * oddways) % MOD);
     }
 
-    public static int Power(long base , long exp){
+    public static long Power(long base , long exp){
         long result = 1;
 
         while(exp > 0){
             if(exp % 2 == 1){
                 result = (result * base) % MOD;
             }
-            base = (base * base)% MOD;
+            base = (base * base) % MOD;
             
             exp = exp/2;
         }
         return result;
     }
     public static void main(String[] args) {
-        long num = 1;
-        System.out.println(CountGoodNumbers(num));
+        long num = 4;
+        System.out.println(CountGoodNumbers(num)); // for num = 4 , output = 400
     }
 }
 
